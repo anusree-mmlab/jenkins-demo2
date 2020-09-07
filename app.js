9a54c24464a722f53747b068a54261528f2acd96
anusree j <anusree.k.gopi@gmail.com>
2020-09-07 17:48:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const math = {};
+
+math.add = (num1, num2) => {
+    return num1 + num2;
+}
+
+math.multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+module.exports = math;
---
+++ test/app.test.js
@@ -0,0 +1,16 @@
+const math = require('../app');
+const assert = require('assert');
+
+describe('Math Library', () => {
+    it('Sum is 3', () => {
+        const res = math.add(1, 2);
+
+        assert.equal(res, 3);
+    });
+
+    it('Product is 18', () => {
+        const res = math.multiply(9, 2);
+
+        assert.equal(res, 18);
+    })
+});
